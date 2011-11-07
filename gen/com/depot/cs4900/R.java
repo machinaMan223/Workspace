e878a89d9ee95e6558eecfa27729b2a1cd5b19e9
Robert Jenkins <rpjenkins@valdosta.edu>
2011-11-07 14:19:25 -0500

    added user_list xml page

--- gen/com/depot/cs4900/R.java
+++ gen/com/depot/cs4900/R.java
@@ -18,11 +18,15 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int address=0x7f07000b;
+        public static final int address_label=0x7f07000a;
         public static final int catalogbutton=0x7f070002;
         public static final int empty=0x7f070000;
         public static final int flipper=0x7f070001;
+        public static final int intro_blurb_users=0x7f070008;
         public static final int local=0x7f070007;
         public static final int mode=0x7f070005;
+        public static final int name=0x7f070009;
         public static final int ordersbutton=0x7f070003;
         public static final int synch=0x7f070006;
         public static final int usersbutton=0x7f070004;
@@ -32,20 +36,23 @@ public final class R {
         public static final int catalog_by_price=0x7f030001;
         public static final int catalog_by_title=0x7f030002;
         public static final int main=0x7f030003;
+        public static final int user_list=0x7f030004;
     }
     public static final class string {
+        public static final int address_entry_label=0x7f050005;
         public static final int animation_text_1=0x7f050002;
         public static final int animation_text_2=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int app_short_name=0x7f050001;
-        public static final int auto_synch=0x7f050004;
-        public static final int catalog=0x7f050007;
-        public static final int catalog_by_popularity=0x7f05000b;
-        public static final int catalog_by_price=0x7f05000a;
-        public static final int catalog_by_title=0x7f050009;
-        public static final int local_only=0x7f050005;
-        public static final int menu_settings=0x7f050006;
-        public static final int user=0x7f050008;
+        public static final int auto_synch=0x7f050006;
+        public static final int catalog=0x7f050009;
+        public static final int catalog_by_popularity=0x7f05000d;
+        public static final int catalog_by_price=0x7f05000c;
+        public static final int catalog_by_title=0x7f05000b;
+        public static final int intro_blurb_users=0x7f050004;
+        public static final int local_only=0x7f050007;
+        public static final int menu_settings=0x7f050008;
+        public static final int user=0x7f05000a;
     }
     public static final class style {
         /**   this is the style for the container ListView, not the rows 
