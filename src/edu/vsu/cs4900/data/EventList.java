ea674acb02da75409f409d3811814885b857c552
Robert Jenkins <rpjenkins@valdosta.edu>
2011-11-10 13:00:31 -0500

    Added first draft of event files including eventEntry, eventList, and eventHandler

--- src/edu/vsu/cs4900/data/EventList.java
+++ src/edu/vsu/cs4900/data/EventList.java
@@ -1,5 +1,111 @@
 package edu.vsu.cs4900.data;
 
-public class EventList {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Vector;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.content.Context;
+import android.util.Log;
+
+import edu.vsu.cs4900.Constants;
+import edu.vsu.cs4900.data.EventEntry;
+import edu.vsu.cs4900.data.EventList;
+import edu.vsu.cs4900.data.EventListHandler;
+
+public class EventList {
+	private static final String CLASSTAG = EventList.class.getSimpleName();
+    private Context _context = null;
+    private List<EventEntry> _eventlist;
+
+    EventList(Context c) {
+        this._context = c;
+        this._eventlist = new Vector<EventEntry>(0);
+    }
+
+    int addEventEntry(EventEntry eventEntry) {
+        this._eventlist.add(eventEntry);
+        return this._eventlist.size();
+    }
+
+    EventEntry getEventEntry(int location) {
+        return this._eventlist.get(location);
+    }
+
+    public List<EventEntry> getAllEventEntries() {
+        return this._eventlist;
+    }
+
+    int getEventEntryCount() {
+        return this._eventlist.size();
+    }
+
+    void replace(EventEntry newEventEntry) {
+        try {
+            EventList newlist = new EventList(this._context);
+            for (int i = 0; i < getEventEntryCount(); i++) {
+            	EventEntry ee = getEventEntry(i);
+                if (ee.get_event_id().equals(newEventEntry.get_event_id())) {
+                    Log.d(Constants.LOGTAG, " " + EventList.CLASSTAG + "Replacing EventEntry");
+                    newlist.addEventEntry(newEventEntry);
+                } else {
+                    newlist.addEventEntry(ee);
+                }
+            }
+            this._eventlist = newlist._eventlist;
+            persist();
+        } catch (Exception e) {
+
+        }
+    }
+
+    // Write to the XML file
+    public void persist() {
+        try {
+            FileOutputStream fos = this._context.openFileOutput(Constants.EVENTS_XML_FILE_NAME, Context.MODE_PRIVATE);
+            fos.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n".getBytes());
+            fos.write("<events>\n".getBytes());            
+            for (int i = 0; i < getEventEntryCount(); i++) {
+            	EventEntry ee = getEventEntry(i);
+                fos.write(ee.toXMLString().getBytes());
+            }
+            fos.write("</events>\n".getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            Log.d(Constants.LOGTAG, " " + EventList.CLASSTAG + "Failed to write out file?" + e.getMessage());
+        }
+    }
+
+    // Read from the XML file
+    public static EventList parse(Context context) {
+        try {
+            FileInputStream fis = context.openFileInput(Constants.EVENTS_XML_FILE_NAME);
+
+            if (fis == null) {
+                return null;
+            }
+            
+            InputSource is = new InputSource(fis);
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            XMLReader xmlreader = parser.getXMLReader();
+            EventListHandler eventHandler = new EventListHandler(context, null);
+            xmlreader.setContentHandler(eventHandler);
+            xmlreader.parse(is);
+            fis.close();
+
+            // return our new eventlist
+            return eventHandler.getList();
+        } catch (Exception e) {
+            Log.d(Constants.LOGTAG, " " + EventList.CLASSTAG + "Error parsing event list xml file: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/edu/vsu/cs4900/data/EventListHandler.java
+++ src/edu/vsu/cs4900/data/EventListHandler.java
@@ -1,7 +1,156 @@
 package edu.vsu.cs4900.data;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import edu.vsu.cs4900.Constants;
+import edu.vsu.cs4900.data.EventEntry;
+import edu.vsu.cs4900.data.EventList;
+import edu.vsu.cs4900.data.EventListHandler;
+
 public class EventListHandler extends DefaultHandler {
+	private static final String CLASSTAG = EventListHandler.class.getSimpleName();
+    Handler ehandler = null;
+    EventList _list;
+    EventEntry _event;
+    String _lastElementName = "";
+    StringBuilder sb = null;
+    Context _context;
+
+    public EventListHandler(Context c, Handler progresshandler) {
+        this._context = c;
+        if (progresshandler != null) {
+            this.ehandler = progresshandler;
+            Message msg = new Message();
+            msg.what = 0;
+            msg.obj = ("Processing Event List");
+            this.ehandler.sendMessage(msg);
+        }
+    }
+
+    public EventList getList() {
+        Message msg = new Message();
+        msg.what = 0;
+        msg.obj = ("Fetching List");
+        if (this.ehandler != null) {
+            this.ehandler.sendMessage(msg);
+        }
+        return this._list;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        Message msg = new Message();
+        msg.what = 0;
+        msg.obj = ("Starting Document");
+        if (this.ehandler != null) {
+            this.ehandler.sendMessage(msg);
+        }
+
+        // initialize our EventLIst object - this will hold our parsed contents
+        this._list = new EventList(this._context);
+
+        // initialize the EventEntry object
+        this._event = new EventEntry();
+
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        Message msg = new Message();
+        msg.what = 0;
+        msg.obj = ("End of Document");
+        if (this.ehandler != null) {
+            this.ehandler.sendMessage(msg);
+        }
+
+    }
+
+    @Override
+    public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+        try {
+            this.sb = new StringBuilder("");
+
+            if (localName.equals("event")) {
+                // create a new item
+
+                Message msg = new Message();
+                msg.what = 0;
+                msg.obj = (localName);
+                if (this.ehandler != null) {
+                    this.ehandler.sendMessage(msg);
+                }
+
+                this._event = new EventEntry();
+
+            }
+        } catch (Exception ee) {
+            Log.d(Constants.LOGTAG, " " + EventListHandler.CLASSTAG + ee.getStackTrace().toString());
+        }
+    }
+
+    @Override
+    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+
+        if (localName.equals("event")) {
+            // add our event to the list!
+            this._list.addEventEntry(this._event);
+            Message msg = new Message();
+            msg.what = 0;
+            msg.obj = ("Storing Event # " + this._event.get_event_id());
+            if (this.ehandler != null) {
+                this.ehandler.sendMessage(msg);
+            }
+
+            return;
+        }
+
+        if (localName.equals("id")) {
+            this._event.set_event_id(this.sb.toString());
+            return;
+        }
+        if (localName.equals("name")) {
+            this._event.set_name(this.sb.toString());
+            return;
+        }
+        if (localName.equals("start_at")) {
+            this._event.set_start_at(this.sb.toString());
+            return;
+        }
+        if (localName.equals("end_at")) {
+            this._event.set_end_at(this.sb.toString());
+            return;
+        }
+        if (localName.equals("location")) {
+            this._event.set_location(this.sb.toString());
+            return;
+        }
+        if (localName.equals("address")) {
+            this._event.set_address(this.sb.toString());
+            return;
+        }
+        if (localName.equals("description")) {
+            this._event.set_description(this.sb.toString());
+            return;
+        }
+        if (localName.equals("price")) {
+            this._event.set_price(this.sb.toString());
+            return;
+        }
+
+    }
+
+    @Override
+    public void characters(char ch[], int start, int length) {
+        String theString = new String(ch, start, length);
+       // Log.d(Constants.LOGTAG, " " + EventListHandler.CLASSTAG + "characters[" + theString + "]");
+        this.sb.append(theString);
+    }
 
 }
