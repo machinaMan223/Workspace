aef663bfd6368f1fb20414f339bbdccb07ee9501
Kai Tillman <kjtillma@valdosta.edu>
2011-11-10 14:58:48 -0500

    Implemented admin pages.

--- src/edu/vsu/cs4900/AdminProductsList.java
+++ src/edu/vsu/cs4900/AdminProductsList.java
@@ -1,13 +1,67 @@
 package edu.vsu.cs4900;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.List;
 
-public class AdminProductsList extends Activity {
+import edu.vsu.cs4900.data.CatalogEntry;
+import edu.vsu.cs4900.data.CatalogList;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class AdminProductsList extends ListActivity {
+private static final String CLASSTAG = AdminProductsList.class.getSimpleName();
+	
+	private TextView empty;
+	private ProgressDialog progressDialog;
+	private CatalogAdapter catalogAdapter;
+	private List<CatalogEntry> catalog;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.acm_admin_products);
+		Log.v(Constants.LOGTAG, " " + AdminProductsList.CLASSTAG + " onCreate");
+		setContentView(R.layout.acm_store);
+		
+		this.empty = (TextView) findViewById(R.id.empty);
+
+		// set list properties
+		final ListView listView = getListView();
+		listView.setItemsCanFocus(false);
+		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		listView.setEmptyView(this.empty);
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.v(Constants.LOGTAG, " " + AdminProductsList.CLASSTAG + " onResume");
+
+		// Parse the data from catalog.xml file
+		catalog = CatalogList.parse(this).getAllCatalogEntries();
+		catalogAdapter = new CatalogAdapter(AdminProductsList.this, catalog, 0); // 0:
+																																					// Sort
+																																					// by
+																																					// title
+		setListAdapter(catalogAdapter);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// // set the current review to the Application (global state placed there)
+		// RestaurantFinderApplication application = (RestaurantFinderApplication)
+		// getApplication();
+		// application.setCurrentReview(this.reviews.get(position));
+		//
+		// // startFrom page is not stored in application, for example purposes it's
+		// a simple "extra"
+		// Intent intent = new Intent(Constants.INTENT_ACTION_VIEW_DETAIL);
+		// intent.putExtra(Constants.STARTFROM_EXTRA,
+		// getIntent().getIntExtra(Constants.STARTFROM_EXTRA, 1));
+		// startActivity(intent);
+	}
 }
--- src/edu/vsu/cs4900/AdminUsersList.java
+++ src/edu/vsu/cs4900/AdminUsersList.java
@@ -1,13 +1,100 @@
 package edu.vsu.cs4900;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.List;
 
-public class AdminUsersList extends Activity {
+import edu.vsu.cs4900.data.UsersEntry;
+import edu.vsu.cs4900.data.UsersList;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class AdminUsersList extends ListActivity {
+
+	// private static final String CLASSTAG =
+	// UsersByTitle.class.getSimpleName();
+	// private static final int MENU_CHANGE_CRITERIA = Menu.FIRST + 1;
+	// private static final int MENU_GET_NEXT_PAGE = Menu.FIRST;
+	// private static final int NUM_RESULTS_PER_PAGE = 8;
+
+	private TextView empty;
+	private ProgressDialog progressDialog;
+	private RosterAdapter rosterAdapter;
+	private List<UsersEntry> users;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.acm_admin_users);
+		setContentView(R.layout.acm_roster_list);
+
+		this.empty = (TextView) findViewById(R.id.empty);
+
+		final ListView listView = getListView();
+		listView.setItemsCanFocus(false);
+		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		listView.setEmptyView(this.empty);
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Parse the data from users.xml file
+		users = UsersList.parse(this).getAllUsersEntries();
+		rosterAdapter = new RosterAdapter(AdminUsersList.this, users, 0); // 0: Sort
+																		// by
+																		// title
+		setListAdapter(rosterAdapter);
+	}
+
+	//
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// super.onCreateOptionsMenu(menu);
+	// menu.add(0, ReviewList.MENU_GET_NEXT_PAGE, 0,
+	// R.string.menu_get_next_page).setIcon(
+	// android.R.drawable.ic_menu_more);
+	// menu.add(0, ReviewList.MENU_CHANGE_CRITERIA, 0,
+	// R.string.menu_change_criteria).setIcon(
+	// android.R.drawable.ic_menu_edit);
+	// return true;
+	// }
+	//
+	// @Override
+	// public boolean onMenuItemSelected(int featureId, MenuItem item) {
+	// Intent intent = null;
+	// switch (item.getItemId()) {
+	// case MENU_GET_NEXT_PAGE:
+	// // increment the startFrom value and call this Activity again
+	// intent = new Intent(Constants.INTENT_ACTION_VIEW_LIST);
+	// intent.putExtra(Constants.STARTFROM_EXTRA,
+	// getIntent().getIntExtra(Constants.STARTFROM_EXTRA, 1)
+	// + ReviewList.NUM_RESULTS_PER_PAGE);
+	// startActivity(intent);
+	// return true;
+	// case MENU_CHANGE_CRITERIA:
+	// intent = new Intent(this, ReviewCriteria.class);
+	// startActivity(intent);
+	// return true;
+	// }
+	// return super.onMenuItemSelected(featureId, item);
+	// }
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// // set the current review to the Application (global state placed
+		// there)
+		// RestaurantFinderApplication application =
+		// (RestaurantFinderApplication) getApplication();
+		// application.setCurrentReview(this.reviews.get(position));
+		//
+		// // startFrom page is not stored in application, for example purposes
+		// it's a simple "extra"
+		// Intent intent = new Intent(Constants.INTENT_ACTION_VIEW_DETAIL);
+		// intent.putExtra(Constants.STARTFROM_EXTRA,
+		// getIntent().getIntExtra(Constants.STARTFROM_EXTRA, 1));
+		// startActivity(intent);
+	}
 }
