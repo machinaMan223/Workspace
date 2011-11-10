ce440443782af59f6a50efffa5dddb1cc0815338
Kai Tillman <kjtillma@valdosta.edu>
2011-11-10 11:50:33 -0500

    Implemented Orders list activity.

--- src/edu/vsu/cs4900/AdminOrdersList.java
+++ src/edu/vsu/cs4900/AdminOrdersList.java
@@ -1,13 +1,64 @@
 package edu.vsu.cs4900;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.List;
 
-public class AdminOrdersList extends Activity {
+import edu.vsu.cs4900.data.OrderEntry;
+import edu.vsu.cs4900.data.OrderList;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class AdminOrdersList extends ListActivity {
+private static final String CLASSTAG = AdminOrdersList.class.getSimpleName();
+	
+	private TextView empty;
+	private ProgressDialog progressDialog;
+	private OrderAdapter orderAdapter;
+	private List<OrderEntry> orders;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.v(Constants.LOGTAG, " " + AdminOrdersList.CLASSTAG + " onCreate");
 		setContentView(R.layout.acm_admin_orders);
-	}
+		
+		this.empty = (TextView) findViewById(R.id.empty);
 
+		// set list properties
+		final ListView listView = getListView();
+		listView.setItemsCanFocus(false);
+		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		listView.setEmptyView(this.empty);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.v(Constants.LOGTAG, " " + AdminOrdersList.CLASSTAG + " onResume");
+
+		// Parse the data from catalog.xml file
+		orders = OrderList.parse(this).getAllOrderEntries();
+		orderAdapter = new OrderAdapter(AdminOrdersList.this, orders); 
+		
+		setListAdapter(orderAdapter);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// // set the current review to the Application (global state placed there)
+		// RestaurantFinderApplication application = (RestaurantFinderApplication)
+		// getApplication();
+		// application.setCurrentReview(this.reviews.get(position));
+		//
+		// // startFrom page is not stored in application, for example purposes it's
+		// a simple "extra"
+		// Intent intent = new Intent(Constants.INTENT_ACTION_VIEW_DETAIL);
+		// intent.putExtra(Constants.STARTFROM_EXTRA,
+		// getIntent().getIntExtra(Constants.STARTFROM_EXTRA, 1));
+		// startActivity(intent);
+	}
 }
--- src/edu/vsu/cs4900/OrderAdapter.java
+++ src/edu/vsu/cs4900/OrderAdapter.java
@@ -1,33 +1,77 @@
 package edu.vsu.cs4900;
 
+import java.util.List;
+
+import edu.vsu.cs4900.data.OrderEntry;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OrderAdapter extends BaseAdapter {
+	private static final String CLASSTAG = OrderAdapter.class.getSimpleName();
+	private final Context context;
+	private final List<OrderEntry> orders;
+
+	public OrderAdapter(Context context, List<OrderEntry> orders) {
+		this.context = context;
+		this.orders = orders;
+		
+		Log.v(Constants.LOGTAG, " " + OrderAdapter.CLASSTAG + " catalog size - "
+				+ this.orders.size());
+	}
 
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.orders.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.orders.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+		OrderEntry oe = this.orders.get(position);
+		return new OrderListView(this.context, oe.get_order_id(), oe.get_name());
 	}
 
+	private final class OrderListView extends LinearLayout {
+		private TextView id;
+		private TextView name;
+		
+		public OrderListView(Context context, String id, String name) {
+			super(context);
+			setOrientation(LinearLayout.VERTICAL);
+
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					ViewGroup.LayoutParams.WRAP_CONTENT,
+					ViewGroup.LayoutParams.WRAP_CONTENT);
+			params.setMargins(5, 3, 5, 0);
+			
+			this.id = new TextView(context);
+			this.id.setText(id);
+			this.id.setTextSize(16f);
+			this.id.setTextColor(Color.WHITE);
+			this.addView(this.id, params);
+			
+			this.name = new TextView(context);
+			this.name.setText(name);
+			this.name.setTextSize(16f);
+			this.name.setTextColor(Color.GRAY);
+			this.addView(this.name, params);
+		}
+		
+	}
 }
